37f0fd290d8ea7a8780e9fc76430ad8b4fe0835a
Emilie <emilie.nilsen@hiof.no>
2020-10-07 18:39:49 +0200

    1. Opprettet bursdags-sidene og lagt inn de fleste elementene som skal være der. 2. Opprettet matplan-sidene og lagt inn de elementene jeg kunne legge inn.

---
+++ app/src/main/java/com/example/mainactivity/BursdagLeggTil.java
@@ -0,0 +1,14 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BursdagLeggTil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bursdag_legg_til);
+    }
+}
---
+++ app/src/main/java/com/example/mainactivity/MatplanLeggTil.java
@@ -0,0 +1,14 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatplanLeggTil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matplan_legg_til);
+    }
+}
