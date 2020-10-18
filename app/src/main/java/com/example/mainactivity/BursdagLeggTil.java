e90bd6f99ba09571252cca37ac062ffd2e08fdbd
Emilie <emilie.nilsen@hiof.no>
2020-10-18 20:27:43 +0200

    Prøvd å få opprettet hver bursdag som cardview

--- app/src/main/java/com/example/mainactivity/BursdagLeggTil.java
+++ app/src/main/java/com/example/mainactivity/BursdagLeggTil.java
@@ -2,55 +2,52 @@ package com.example.mainactivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.Date;
+import java.util.ArrayList;
 
 public class BursdagLeggTil extends Activity {
+
+    private Button lagre = findViewById(R.id.BirthdayLagre);
+    private EditText FullName = findViewById(R.id.BirthdayFullName);
+    private EditText Birthday = findViewById(R.id.BirthdayDate);
+    private EditText PhoneNumber = findViewById(R.id.BirthdayPhoneNumber);
 
     public static final String KEY_NAME = "edit_Name";
     public static final String KEY_PHONE = "edit_Phone";
     public static final String KEY_DATE = "edit_Date";
 
-
-    private Button lagre;
-    private EditText FullName;
-    private EditText Birthday;
-    private EditText PhoneNumber;
+    ArrayList<String> Name = new ArrayList<String>();
+    ArrayList<String> Phone = new ArrayList<String>();
+    ArrayList<String> Date = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bursdag_legg_til);
 
-        FullName = findViewById(R.id.BirthdayFullName);
-        Birthday = findViewById(R.id.BirthdayDate);
-        PhoneNumber = findViewById(R.id.BirthdayPhoneNumber);
-        lagre = findViewById(R.id.BirtdayLagre);
-        lagre.setOnClickListener(new View.OnClickListener() {
+       lagre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Name.add(FullName.getText().toString());
+                Phone.add(PhoneNumber.getText().toString());
+                Date.add(Birthday.getText().toString());
+
                 Intent intent = new Intent(v.getContext(), Bursdager.class);
-
-                String PersonName = FullName.getText().toString();
-                String Phone = PhoneNumber.getText().toString();
-                String BirthdayDate = Birthday.getText().toString();
-
-                intent.putExtra(KEY_NAME, PersonName);
+                intent.putExtra(KEY_NAME, Name);
                 intent.putExtra(KEY_PHONE, Phone);
-                intent.putExtra(KEY_DATE, BirthdayDate);
+                intent.putExtra(KEY_DATE, Date);
+
                 startActivity(intent);
+
             }
         });
 
-
         endActivityAndGoBack(R.id.tilbakeBtn);
         endActivityAndGoBack(R.id.avbrytBtn);
-
-
 
     }
 }
--- app/src/main/java/com/example/mainactivity/Bursdager.java
+++ app/src/main/java/com/example/mainactivity/Bursdager.java
@@ -1,19 +1,23 @@
 package com.example.mainactivity;
 
 import android.os.Bundle;
-
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Bursdager extends Activity {
-
+    private RecyclerView recyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bursdager);
+        recyclerView = findViewById(R.id.BirthdayListRecycler);
 
         endActivityAndGoBack(R.id.tilbakeBtn);
         goToNewSiteListener(R.id.AddBirthday, BursdagLeggTil.class);
 
     }
+
+
+
 }
