76563e5fec2f61425aec9f29bab48c57ee5d49c0
Sandnes <Aleksander.sandnes@hotmail.com>
2020-10-18 19:50:34 +0200

    logg ut knapper

--- app/src/main/java/com/example/mainactivity/ChangeProfileActivity.java
+++ app/src/main/java/com/example/mainactivity/ChangeProfileActivity.java
@@ -1,4 +1,4 @@
 package com.example.mainactivity;
 
-public class ChangeProfileActivity {
+public class ChangeProfileActivity extends Activity {
 }
--- app/src/main/java/com/example/mainactivity/HandlelisteOversiktActivity.java
+++ app/src/main/java/com/example/mainactivity/HandlelisteOversiktActivity.java
@@ -1,4 +1,20 @@
 package com.example.mainactivity;
 
-public class HandlelisteOversiktActivity {
+import android.os.Bundle;
+
+public class HandlelisteOversiktActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handleliste_oversikt);
+
+        endActivityAndGoBack(R.id.tilbakeBtn);
+
+        goToNewSiteListener(R.id.btnHandleliste1, HandlelisteEnListeTemp.class);
+        goToNewSiteListener(R.id.btnHandleliste2, HandlelisteEnListeTemp.class);
+        goToNewSiteListener(R.id.btnHandleliste3, HandlelisteEnListeTemp.class);
+        goToNewSiteListener(R.id.btnHandleliste4, HandlelisteEnListeTemp.class);
+        goToNewSiteListener(R.id.opprettNyListe, OpprettHandlelisteActivity.class);
+    }
 }
