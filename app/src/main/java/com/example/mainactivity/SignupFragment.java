0dc6c32d7df3230925b6bae385b7f5798ebe5410
Sandnes <Aleksander.sandnes@hotmail.com>
2020-10-21 23:38:56 +0200

    Oppdaterte signupsiden til fragment

--- app/src/main/java/com/example/mainactivity/SignupFragment.java
+++ app/src/main/java/com/example/mainactivity/SignupFragment.java
@@ -12,15 +12,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class SignupFragment extends Fragment {
 
     Database database;
     User user;
 
-    private Button registrerBruker;
-    private ImageButton tilbake;
     private EditText aName, anEmail, aPassword, aPasswordConfirm, aBirthday, aMobilnr;
+
     public SignupFragment() {
     }
 
@@ -33,8 +33,8 @@ public class SignupFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        registrerBruker = view.findViewById(R.id.SignupRegistrerBruker);
-        tilbake = view.findViewById(R.id.SignupTilbake);
+        Button registrerBruker = view.findViewById(R.id.SignupRegistrerBruker);
+        ImageButton tilbake = view.findViewById(R.id.SignupTilbake);
         aName = view.findViewById(R.id.SignupNavnInput);
         anEmail = view.findViewById(R.id.SignupEmailInput);
         aMobilnr = view.findViewById(R.id.SignupMobilnummerInput);
@@ -44,6 +44,48 @@ public class SignupFragment extends Fragment {
 
         registrerBruker.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_signupFragment_to_hovedsideFragment));
         tilbake.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_signupFragment_to_loginFragment));
+        
+        database = new Database(getActivity());
 
+        registrerBruker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = aName.getText().toString();
+                String email = anEmail.getText().toString();
+                String birthday = aBirthday.getText().toString();
+                String mobilnr = String.valueOf(aMobilnr);
+                String password = aPassword.getText().toString();
+                String passwordConfirm = aPasswordConfirm.getText().toString();
+
+                if (validUserInfo(name, email, birthday, mobilnr, password, passwordConfirm)) {
+                    AddUser(name, email, birthday , mobilnr, password);
+                    aName.setText("");
+                    anEmail.setText("");
+                    aPassword.setText("");
+                    aPasswordConfirm.setText("");
+                    aBirthday.setText("");
+                    aMobilnr.setText("");
+                } else {
+                    Toast.makeText(getActivity(), "You must put something in the text field", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void AddUser(String name, String email, String birthday, String mobilnr, String password) {
+        boolean insertData = database.addUserToDatabase(name, email, birthday, mobilnr, password);
+
+        if (insertData)
+            Toast.makeText(getActivity(), "Data successfully inserted", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getActivity(), "Something went wrong", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validUserInfo(String name, String email, String birthday, String mobilnr, String password, String passwordConfirm) {
+        if (name.length() == 0 || email.length() == 0 || password.length() == 0 || passwordConfirm.length() == 0 || birthday.length() == 0 || mobilnr.length() == 0)
+            return false;
+        return password.equals(passwordConfirm);
+
+        //Kan evt. legge in regEx på email etterhvert.
     }
 }
