623af6cf11acbf329bf69acdb5050130c8b01126
Emilie <emilie.nilsen@hiof.no>
2020-11-09 18:08:10 +0100

    Laget de tre sidene som trengs for opprett bruker. Satt sammen i navgrapgh. Ikke lagd funksjonene

---
+++ app/src/main/java/com/example/mainactivity/FamilieFragment.java
@@ -0,0 +1,22 @@
+package com.example.mainactivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FamilieFragment extends Fragment {
+
+    public FamilieFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_familie, container, false);
+    }
+}
