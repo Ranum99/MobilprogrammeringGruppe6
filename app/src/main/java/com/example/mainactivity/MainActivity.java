18fa4a34a21e5922f32f8aab9579cc7edee74e4e
Emilie <emilie.nilsen@hiof.no>
2020-12-02 15:11:18 +0100

    Navigation drawer, + småting

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         NavController controller = Navigation.findNavController(this, R.id.fragment);
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
         NavigationUI.setupWithNavController(bottomNavigation, controller);
+
+        NavigationView navigationView = findViewById(R.id.NavigationView);
+        NavigationUI.setupWithNavController(navigationView, controller);
 
 
     }
