c8ada3f3e1789a0ef786f9d3278d325c66c5679a
Emilie <emilie.nilsen@hiof.no>
2020-12-07 20:04:03 +0100

    småting

--- app/src/main/java/com/example/mainactivity/BursdagFragment.java
+++ app/src/main/java/com/example/mainactivity/BursdagFragment.java
@@ -46,6 +46,8 @@ public class BursdagFragment extends Fragment{
     // ArrayList for å lagre dataen fra databasen
     private ArrayList<BirthdayModel> bursdager = new ArrayList<>();
 
+    private Integer splitAarc1, splitMaanedc1, splitDagc1, splitAarc2, splitMaanedc2, splitDagc2;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -91,9 +93,17 @@ public class BursdagFragment extends Fragment{
         while(data.moveToNext()) {
 
             String navnet = data.getString(data.getColumnIndex(Database.COLUMN_NAME_BIRTHDAY));
+            System.out.println(navnet);
             String datoen = data.getString(data.getColumnIndex(Database.COLUMN_BIRTHDAY_DATE));
+            System.out.println(datoen);
             String id = data.getString(data.getColumnIndex(Database.COLUMN_ID));
+<<<<<<< Updated upstream
             String familieId = data.getString(data.getColumnIndex(Database.COLUMN_BIRTHDAY_FAMILYID));
+=======
+            System.out.println(id);
+            String familieId = data.getString(data.getColumnIndex(Database.COLUMN_BIRTHDAY_FAMILYID));
+            System.out.println(familieId);
+>>>>>>> Stashed changes
 
             System.out.println(navnet + " - " + datoen + " - " + id + " - " + familieId);
 
@@ -114,8 +124,21 @@ public class BursdagFragment extends Fragment{
         Comparator<BirthdayModel> byBirthday = new Comparator<BirthdayModel>() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             public int compare(BirthdayModel c1, BirthdayModel c2) {
-                DateTimeFormatter format = DateTimeFormatter.ofPattern("d.M.uuuu");
-                return LocalDate.parse(c1.getDato(), format).compareTo(LocalDate.parse(c2.getDato(), format));
+                DateTimeFormatter format = DateTimeFormatter.ofPattern("d.M");
+
+                String[] partsc1 = c1.getDato().split("\\.");
+                splitAarc1 = Integer.parseInt(partsc1[2]);
+                splitMaanedc1 = Integer.parseInt(partsc1[1]);
+                splitDagc1 = Integer.parseInt(partsc1[0]);
+                String[] partsc2 = c2.getDato().split("\\.");
+                splitAarc2 = Integer.parseInt(partsc2[2]);
+                splitMaanedc2 = Integer.parseInt(partsc2[1]);
+                splitDagc2 = Integer.parseInt(partsc2[0]);
+
+                String c1Dato = String.valueOf(splitDagc1) + String.valueOf(splitMaanedc1);
+                String c2Dato = String.valueOf(splitDagc2) + String.valueOf(splitMaanedc2);
+
+                return LocalDate.parse(c1Dato, format).compareTo(LocalDate.parse(c2Dato, format));
             }
         };
         Collections.sort(bursdager, byBirthday);
