077a39965c66a24bda02933e308ff5d43ca9a73b
Sandnes <Aleksander.sandnes@hotmail.com>
2020-12-12 17:52:04 +0100

    Intent filter

--- app/src/main/java/com/example/mainactivity/SignupFragment.java
+++ app/src/main/java/com/example/mainactivity/SignupFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +29,8 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static android.app.Activity.RESULT_OK;
+
 public class SignupFragment extends Fragment {
     public SignupFragment() {
         // Required empty constructor
@@ -38,8 +44,6 @@ public class SignupFragment extends Fragment {
     private DatePicker aBirthday;
     private Button registrerBruker;
     private ImageView logo;
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
