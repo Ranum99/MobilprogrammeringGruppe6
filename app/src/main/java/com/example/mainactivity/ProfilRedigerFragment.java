1adc146b1459d25fcd25ca49cc770bff19721187
Sandnes <Aleksander.sandnes@hotmail.com>
2020-12-13 17:50:38 +0100

    Bilder og fjernet nav ting

--- app/src/main/java/com/example/mainactivity/ProfilRedigerFragment.java
+++ app/src/main/java/com/example/mainactivity/ProfilRedigerFragment.java
@@ -1,17 +1,26 @@
 package com.example.mainactivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +29,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class ProfilRedigerFragment extends Fragment {
@@ -30,6 +40,9 @@ public class ProfilRedigerFragment extends Fragment {
     private EditText endreNavn, endreEmail, endreMobilnr;
     private DatePicker endreBursdag;
     private String navn, mobil, email, dato, id;
+    private Button btnChangePicture;
+    // PICK_PHOTO_CODE is a constant integer
+    public final static int PICK_PHOTO_CODE = 1046;
 
     public ProfilRedigerFragment() {
     }
@@ -43,6 +56,15 @@ public class ProfilRedigerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.requireActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.requireActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+
         final NavController navController = Navigation.findNavController(requireActivity(), R.id.fragment);
 
         // Instansierer variabler
@@ -53,9 +75,7 @@ public class ProfilRedigerFragment extends Fragment {
         endreMobilnr = view.findViewById(R.id.profilMobilnr);
         database = new Database(getActivity());
         sharedPreferences = requireContext().getSharedPreferences(User.SESSION, Context.MODE_PRIVATE);
-        ImageView photo = view.findViewById(R.id.profilbildeRediger);
-
-        photo.setImageResource(R.drawable.ic_baseline_account_circle_24);
+        btnChangePicture = view.findViewById(R.id.btnChangePicture);
 
         assert getArguments() != null;
         endreNavn.setText(getArguments().getString("NAVN"));
@@ -85,5 +105,59 @@ public class ProfilRedigerFragment extends Fragment {
                 navController.navigateUp();
             }
         });
+
+        btnChangePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                onPickPhoto(view1);
+            }
+        });
+    }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(requireActivity().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(this.requireActivity().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(this.requireActivity().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = loadFromUri(photoUri);
+
+            // Load the selected image into a preview
+            ImageView ivPreview = (ImageView) requireView().findViewById(R.id.profilbildeRediger);
+            ivPreview.setImageBitmap(selectedImage);
+        }
     }
 }
